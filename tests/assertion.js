87fc7074452ac7e5a2236707f5343cb0c7453c69
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-11-19 13:07:31 +0200

    tests passing for primary and secondary assertion issuers

--- tests/assertion.js
+++ tests/assertion.js
@@ -6,7 +6,7 @@
 
 const
 should = require('should'),
-browserid = require('../'),
+BrowserID = require('../'),
 IdP = require('./lib/idp.js').IdP,
 Client = require('./lib/client.js'),
 jwcrypto = require('jwcrypto');
@@ -18,27 +18,46 @@ require("jwcrypto/lib/algs/ds");
 describe('assertion verification, basic', function() {
   // a local idp with a 1s delay in serving support documents
   var idp = new IdP();
+  var browserid = new BrowserID({ insecureSSL: true});
+  var client;
 
   it('test idps should start up', function(done) {
     idp.start(done);
   });
 
-  it('validation of basic assertion should succeed', function(done) {
-    // allocate a new "client".  She has an email and idp as specified below
-    var client = new Client({
-      email: 'test@' + idp.domain(),
-      idp: idp
-    });
+  it('validation of basic assertion signed by IdP should succeed', function(done) {
+    client = new Client({ idp: idp });
 
+    // allocate a new "client".  She has an email and idp as specified below
     // generate an assertion (and all pre-requisites)
     client.assertion({ audience: 'http://example.com' }, function(err, assertion) {
       browserid.verify(
-        { insecureSSL: true },
         assertion, 'http://example.com',
         function(err, details) {
+          // XXX: check details for pertinent values
           console.log(details);
           done(err);
         });
+    });
+  });
+
+  it('validation of basic assertion signed by Fallback should succeed', function(done) {
+    client = new Client({
+      idp: idp,
+      email: 'test@example,com'
+    });
+
+    // allocate a new "client".  She has an email and idp as specified below
+    // generate an assertion (and all pre-requisites)
+    client.assertion({ audience: 'http://example.com' }, function(err, assertion) {
+      // XXX: now we must specify a fallback
+      BrowserID.verify({
+        insecureSSL: true,
+        fallback: idp.domain()
+      }, assertion, 'http://example.com', function(err) {
+        // XXX: check details for pertinent values
+        done(err);
+      });
     });
   });
 
