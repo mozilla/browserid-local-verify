b19b53e137d5c1592502370b25d072b3b014b270
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-11-22 12:22:41 +0200

    fully functional command line lookup facility

--- bin/browserid-lookup.js
+++ bin/browserid-lookup.js
@@ -1,25 +1,59 @@
 #!/usr/bin/env node
 
-/* this should be invoked as follows
- *
- * browserid-lookup [args] <domain>
- */
-
 var
-BrowserID = require("..");
+BrowserID = require('../'),
+colors = require('colors');
+
+colors.setTheme({
+  input: 'grey',
+  verbose: 'grey',
+  prompt: 'grey',
+  info: 'green',
+  data: 'grey',
+  help: 'cyan',
+  warn: 'yellow',
+  debug: 'blue',
+  error: 'red'
+});
 
 var args = require('optimist')
-.usage('Determine whether a domain supports BrowserID.\nUsage: $0')
+.usage('Perform BrowserID lookup to determine the authority for a specific domain.\nUsage: $0')
 .alias('h', 'help')
 .describe('h', 'display this usage message')
-.alias('k', 'keylength')
+.alias('f', 'fallback')
+.describe('f', 'domain of fallback IdP (often - login.persona.org)')
+.alias('p', 'principalDomain')
+.describe('p', 'specify the prinicpal email domain')
+.alias('v', 'verbose')
+.boolean('v')
+.describe('v', 'print annoyinglymuch info about the discovery process')
 var argv = args.argv;
 
-if (argv.h) {
+if (argv.h || argv._.length !== 1) {
   args.showHelp();
   process.exit(1);
 }
 
-BrowserID.lookup(process.argv[(process.argv.length - 1)], null, function(err, details) {
-  console.log(err, details);
+var domain = argv._[0];
+var principalDomain = argv.p || domain;
+
+var browserid = new BrowserID({
+  fallback: argv.fallback
+});
+
+if (argv.v) {
+  browserid.on('info', function(msg, data) {
+    console.log(msg, data);
+  });
+}
+
+browserid.lookup(domain, principalDomain, function(err, details) {
+  if (err) {
+    console.log("no support:".error, err);
+  } else {
+    if (argv.v) console.log("\n\n");
+    // convert publicKey to displayable object
+    details.publicKey = details.publicKey.toSimpleObject();
+    console.log(details.authoritativeDomain.info, "is authoritative for", '@' + principalDomain.info, "email addresses:", JSON.stringify(details, null, 2).data);
+  }
 });
