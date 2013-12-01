33ff32fc14ff737517b30b72ee1657d46e1c9094
Lloyd Hilaiel <lloyd@hilaiel.com>
2013-12-01 15:02:36 -0800

    remove uneeded includes

--- tests/domain-authority.js
+++ tests/domain-authority.js
@@ -8,11 +8,7 @@ const
 should = require('should'),
 BrowserID = require('../'),
 IdP = require('./lib/idp.js'),
-Client = require('./lib/client.js'),
-jwcrypto = require('jwcrypto');
-
-require("jwcrypto/lib/algs/rs");
-require("jwcrypto/lib/algs/ds");
+Client = require('./lib/client.js');
 
 describe('domain authority', function() {
   var idp = new IdP();
