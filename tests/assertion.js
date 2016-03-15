a348b404211ecb87ff6b7dc90b073e7f3eea3299
Ryan Kelly <rfkelly@mozilla.com>
2016-03-15 11:02:32 +1100

    Bug 1255697 - Limit characters allowed in email addresses; r=fmarier

--- tests/assertion.js
+++ tests/assertion.js
@@ -154,6 +154,94 @@ describe('assertion verification, basic', function() {
     });
   });
 
+  it('validation of assertion with newlines in the email should fail', function(done) {
+    client = new Client({
+      idp: idp,
+      email: 'test@users.example.com\n@example.com'
+    });
+
+    // allocate a new "client".  She has an email and idp as specified below
+    // generate an assertion (and all pre-requisites)
+    client.assertion({ audience: 'http://example.com' }, function(err, assertion) {
+      browserid.verify({
+        fallback: idp.domain(),
+        httpTimeout: 0.1,
+        assertion: assertion,
+        audience: 'http://example.com'
+      }, function(err) {
+        should.exist(err);
+        err.should.contain("untrusted assertion, doesn't contain an email, and issuer is untrusted");
+        done();
+      });
+    });
+  });
+
+  it('validation of assertion with null bytes in the email hostname should fail', function(done) {
+    client = new Client({
+      idp: idp,
+      email: 'test@users.example\u0000.com'
+    });
+
+    // allocate a new "client".  She has an email and idp as specified below
+    // generate an assertion (and all pre-requisites)
+    client.assertion({ audience: 'http://example.com' }, function(err, assertion) {
+      browserid.verify({
+        fallback: idp.domain(),
+        httpTimeout: 0.1,
+        assertion: assertion,
+        audience: 'http://example.com'
+      }, function(err) {
+        should.exist(err);
+        err.should.contain("untrusted assertion, doesn't contain an email, and issuer is untrusted");
+        done();
+      });
+    });
+  });
+
+  it('validation of assertion with null bytes in the email username should fail', function(done) {
+    client = new Client({
+      idp: idp,
+      email: 'test\u0000@users.example.com'
+    });
+
+    // allocate a new "client".  She has an email and idp as specified below
+    // generate an assertion (and all pre-requisites)
+    client.assertion({ audience: 'http://example.com' }, function(err, assertion) {
+      browserid.verify({
+        fallback: idp.domain(),
+        httpTimeout: 0.1,
+        assertion: assertion,
+        audience: 'http://example.com'
+      }, function(err) {
+        should.exist(err);
+        err.should.contain("untrusted assertion, doesn't contain an email, and issuer is untrusted");
+        done();
+      });
+    });
+  });
+
+  it('validation of assertion with regex-busting newline should fail', function(done) {
+    client = new Client({
+      idp: idp,
+      email: 'test@users.example.com\u0000\n@example.com'
+    });
+
+    // allocate a new "client".  She has an email and idp as specified below
+    // generate an assertion (and all pre-requisites)
+    client.assertion({ audience: 'http://example.com' }, function(err, assertion) {
+      browserid.verify({
+        fallback: idp.domain(),
+        httpTimeout: 0.1,
+        assertion: assertion,
+        audience: 'http://example.com'
+      }, function(err) {
+        should.exist(err);
+        err.should.contain("untrusted assertion, doesn't contain an email, and issuer is untrusted");
+        done();
+      });
+    });
+  });
+
   it('test idp should shut down', function(done) {
     idp.stop(done);
   });
